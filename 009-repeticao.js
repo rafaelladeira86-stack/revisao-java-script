cf8163bcffa85a829114d4cff877d5e9ee0ca97f
rafaelladeira86-stack <rafaelladeira86@gmail.com>
2025-08-25 21:38:51 -0300

    Create 009-repeticao.js

---
+++ 009-repeticao.js
@@ -0,0 +1,12 @@
+// repetição com teste no inicio
+let nInicio = 10;
+let nFim = 20;
+let repeticoes = 0;
+
+
+while (nInicio< nFim){
+    console.log(nInicio);
+    nInicio++;
+}
+
+console.log(`o codigo foi repetido ${repeticoes} vezes!`);
