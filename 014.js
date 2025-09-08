2fbdcac463f4b174ac4818d868972d24ff795bfc
rafaelladeira86-stack <rafaelladeira86@gmail.com>
2025-09-08 20:38:59 -0300

    015-repeticaofinal

---
+++ 014.js
@@ -0,0 +1,28 @@
+let entrada = require('prompt-sync') ();
+
+let frase =  entrada ("Digite uma frase: ");
+let frase2 = '';
+let contador = -1;
+
+while(contador< frase.length) {
+    contador++;
+
+
+    if (frase[contador]=='a'||
+        frase[contador]=='e'||
+        frase[contador]=='i'||
+        frase[contador]=='o'||
+        frase[contador]=='u'||
+        frase[contador]==' '||
+        frase[contador]=='ã'||
+        frase[contador]=='ó'
+    ){
+
+        continue;
+    }
+    frase2 = frase2 + frase.charAt (contador);
+
+    
+}
+
+console.log(frase2);
